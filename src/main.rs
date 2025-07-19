d877950b362e0432f505c8a1cfe24fc141267916
Jarl Sondre Sæther <60541573+jarlsondre@users.noreply.github.com>
2025-07-19 21:16:27 +0200

    refactor main slightly

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 use std::process::Command;
 mod tmux_utils;
 
-// This file serves as the main entry point to the plugin. All it does it launch 
+// This file serves as the main entry point to the plugin. All it does it launch
 // `tmux popup -E <fzf-handler>`. This could probably have been done in bash, but feels nice
 // to keep everything in Rust.
 
-fn main() {
-    let mut fzf_handler_path = match std::env::current_exe() {
-        Ok(val) => val,
-        Err(e) => {
-            println!("Failed to find the current path!");
-            println!("Error was: {:?}", e.to_string());
-            return
-        }
-    };
+fn main() -> Result<(), String> {
+    let mut fzf_handler_path = std::env::current_exe().map_err(|e| {
+        format!(
+            "Getting path of current executable failed with error: {}",
+            e
+        )
+    })?;
     fzf_handler_path.set_file_name("fzf_handler");
 
     let _ = Command::new("tmux")
@@ -21,4 +19,6 @@ fn main() {
         .arg("-E")
         .arg(fzf_handler_path)
         .output();
+
+    Ok(())
 }
